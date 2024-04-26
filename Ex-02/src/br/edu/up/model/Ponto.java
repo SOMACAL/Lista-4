2b35cfe91e2842640579fca88d08aea910918f21
Victor Martins <VictorMLMartins@icloud.com>
2024-04-26 09:06:49 -0300

    feat: :sparkles: Classe Ponto
    Realizado a criação da classe ponto, seu contructor get's, e set's.

---
+++ Ex-02/src/br/edu/up/model/Ponto.java
@@ -0,0 +1,29 @@
+package br.edu.up.model;
+
+public class Ponto {
+  double x;
+  double y;
+
+  Ponto() {}
+
+  Ponto(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public void setX(double x) {
+    this.x = x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public void setY(double y) {
+    this.y = y;
+  }
+}
