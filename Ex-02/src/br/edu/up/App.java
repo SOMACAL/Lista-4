1b6cdbe57cc636cadc5f83b0759610c617ea3142
Victor Martins <VictorMLMartins@icloud.com>
2024-05-11 11:48:05 -0300

    feat: :sparkles: Função Calcular Distancia, Classe App
    Criado a função calcular distância e também realizado os calculos pedidos do exercício

--- Ex-02/src/br/edu/up/App.java
+++ Ex-02/src/br/edu/up/App.java
@@ -1,7 +1,31 @@
 package br.edu.up;
+import br.edu.up.model.Ponto;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		//1
+		Ponto ponto1 = new Ponto();
+		
+		//2
+		Ponto ponto2 = new Ponto(2, 5);
+		
+		//3
+		System.out.println("\n-------------------------");
+		double distancia1ao2 = ponto1.calcularDistancia(ponto1.getX(), ponto1.getY(), ponto2.getX(), ponto2.getY());
+		System.out.println("O valor da disância entre o ponto 1 ao ponto 2 é de: " + distancia1ao2);
+
+		//4
+		System.out.println("\n-------------------------");
+		Ponto ponto3 = new Ponto(7, 2);
+		double distancia2ao3 = ponto2.calcularDistancia(ponto2.getX(), ponto2.getY(), ponto3.getX(), ponto3.getY());
+		System.out.println("O valor da disância entre o ponto 2 ao ponto 3 é de: " + distancia2ao3);
+
+		//5
+		ponto1.setX(10);
+
+		//6
+		ponto1.setY(3);
+		System.out.println("\n-------------------------");
+		System.out.println("As novas coordenadas do ponto 1 é de: x:" + ponto1.getX() + " y: " + ponto1.getY());
+	}
 }
--- Ex-02/src/br/edu/up/model/Ponto.java
+++ Ex-02/src/br/edu/up/model/Ponto.java
@@ -4,7 +4,10 @@ public class Ponto {
   private double x;
   private double y;
 
-  public Ponto() {}
+  public Ponto() {
+    this.x = 0;
+    this.y = 0;
+  }
 
   public Ponto(double x, double y) {
     this.x = x;
@@ -25,5 +28,12 @@ public class Ponto {
 
   public void setY(double y) {
     this.y = y;
+  }
+
+  public double calcularDistancia(double x1, double y1, double x2, double y2) {
+    double xAoQuadrado = Math.pow((x1 - x2), 2);
+    double yAoQuadrado = Math.pow((y1 - y2), 2);
+    double somaXY = xAoQuadrado + yAoQuadrado;
+    return Math.sqrt(somaXY);
   }
 }
