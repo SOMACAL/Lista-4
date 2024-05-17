da63a2db23a5b0a0ad3e3ef9b66f2cd218c3f6a1
Victor Martins <VictorMLMartins@icloud.com>
2024-05-17 09:38:45 -0300

    feat: :sparkles: Implementação View
    Implementado a classe view para os objetos criados.

--- Ex-01/src/br/edu/up/App.java
+++ Ex-01/src/br/edu/up/App.java
@@ -1,30 +1,14 @@
 package br.edu.up;
 import br.edu.up.model.Livro;
+import br.edu.up.view.LivroView;
 
 public class App {
     public static void main(String[] args) {
-        Livro livro1 = new Livro("1598FHK", "Core Java 2", new String[]{"Cay S. Horstmann", "Gary Cornell"}, "0130819336", 2005);
-        Livro livro2 = new Livro("9865PLO", "Java, Como programar", new String[]{"Harvey Deitel"}, "0130341517", 2015);
+        Livro livroCore = new Livro("1598FHK", "Core Java 2", new String[]{"Cay S. Horstmann", "Gary Cornell"}, "0130819336", 2005);
+        Livro livroJava = new Livro("9865PLO", "Java, Como programar", new String[]{"Harvey Deitel"}, "0130341517", 2015);
 
-        System.out.println("\n");
-        System.out.println("Livro 01:");
-        System.out.println("Código: " + livro1.getCodigo());
-        System.out.println("Título: " + livro1.getTitulo());
-        System.out.print("Autores: ");
-        for (String autor : livro1.getAutores()) {
-            System.out.print(autor + ", ");
-        }
-        System.out.println("\nISBN: " + livro1.getISBN());
-        System.out.println("Ano: " + livro1.getAno());
-
-        System.out.println("\nLivro 02:");
-        System.out.println("Código: " + livro2.getCodigo());
-        System.out.println("Título: " + livro2.getTitulo());
-        System.out.print("Autores: ");
-        for (String autor : livro2.getAutores()) {
-            System.out.print(autor + ", ");
-        }
-        System.out.println("\nISBN: " + livro2.getISBN());
-        System.out.println("Ano: " + livro2.getAno());
+        LivroView.visualizar(livroCore, livroCore.getAutores());
+        
+        LivroView.visualizar(livroJava, livroJava.getAutores());
     }
 }
